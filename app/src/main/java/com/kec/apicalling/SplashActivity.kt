725d4db395c53dac999529afb16b3026b3bc4634
aarush368 <61826716+aarush368@users.noreply.github.com>
2022-05-27 17:14:07 +0530

    add splash activity

---
+++ app/src/main/java/com/kec/apicalling/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.kec.apicalling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
